521bc1288e206087301f1551f71a858490f48bf4
Issac Chan <issaczhaoming.chan@my.jcu.edu.au>
2019-03-20 18:33:20 +0800

    Second commit
    Complete exercise

--- src/MovieQuoteInfo.java
+++ src/MovieQuoteInfo.java
@@ -3,15 +3,24 @@ import java.util.Random;
 public class MovieQuoteInfo {
     public static void main(String []args){
         Random randOne = new Random();
-        int q = randOne.nextInt(2); // determines random selection's values, starting from 0 to indicated end number
+        int q = randOne.nextInt(3); // determines random selection's values
         if(q == 0) {
             System.out.println("Tonight, we dine in hell!");
+            System.out.println("300");
+            System.out.println("King Leonidas");
+            System.out.println("2006");
         }
         else if(q == 1) {
             System.out.println("I don't know what we're yelling about!");
+            System.out.println("Anchorman: The Legend of Ron Burgundy");
+            System.out.println("Brick Tamland");
+            System.out.println("2004");
         }
         else {
-            System.out.println("Stand still and die like a man!");
+            System.out.println("Tony, there was no other way.");
+            System.out.println("Avengers: Infinity War");
+            System.out.println("Doctor Strange");
+            System.out.println("2018");
         }
 
     }
